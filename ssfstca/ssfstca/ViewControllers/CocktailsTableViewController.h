6fb00bd756cc04c108af8dcbd843c46e0c6be931
Andreas Bohatsch <abohatsch@me.com>
2015-02-22 18:07:55 +0100

    added codesnipped file

--- ssfstca/ssfstca/ViewControllers/CocktailsTableViewController.h
+++ ssfstca/ssfstca/ViewControllers/CocktailsTableViewController.h
@@ -6,6 +6,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface CocktailsTableViewController : UITableViewController
+@interface CocktailsTableViewController : UITableViewController <UITableViewDelegate>
 
 @end
--- ssfstca/ssfstca/ViewControllers/CocktailsTableViewController.m
+++ ssfstca/ssfstca/ViewControllers/CocktailsTableViewController.m
@@ -8,15 +8,11 @@
 #import "CocktailDataSource.h"
 #import "CocktailCell.h"
 
-#import "ManagedCocktailDataSource.h"
-#import "CocktailManagedObject.h"
 
 @class CocktailObject;
 
 @interface CocktailsTableViewController()
 @property (nonatomic, strong) TableDataSource *dataSource;
-@property (nonatomic, strong) ManagedCocktailDataSource *managedDataSource;
-
 @property (nonatomic, assign) BOOL toggleFlag;
 @end
 
@@ -29,9 +25,7 @@
 {
     [super viewDidLoad];
     
-    //self.dataSource = [self configureDataSource];
-    
-    self.managedDataSource = [self configureManagedDataSource];
+    self.dataSource = [self configureDataSource];
 }
 
 
@@ -51,30 +45,16 @@
     return dataSource;
 }
 
-
-- (ManagedCocktailDataSource *)configureManagedDataSource
+- (void) tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    ManagedCocktailDataSource *dataSource = [[ManagedCocktailDataSource alloc] initWithTableView:self.tableView
-                                                                            cellIdentifier:[CocktailCell reuseIdentifier]
-                                                                        configureCellBlock:^(id cell, id item) {
-                                                                            
-                                                                                CocktailCell *cocktailCell = (CocktailCell *)cell;
-                                                                                CocktailObject *cocktailObj = (CocktailObject *)item;
-                                                                            
-                                                                                [cocktailCell configureWithDataObject:cocktailObj];
-                                                                            }];
-    
-    dataSource.fetchRequest = [CocktailManagedObject defaultFetchRequest];
-
-    return dataSource;
+    NSLog(@"clicked");
 }
-
 
 #pragma mark - ibactions
 
 - (IBAction)loadCocktails:(id)sender
 {
-    [self.managedDataSource loadContent];
+    [self.dataSource loadContent];
 }
 
 
@@ -82,14 +62,12 @@
 {
     self.toggleFlag = !self.toggleFlag;
 
-//    CocktailDataSource *cocktailSource;
-//    if ([self.dataSource isKindOfClass:[CocktailDataSource class]]) {
-//        cocktailSource = (CocktailDataSource *)self.dataSource;
-//    }
-//    
-//    [cocktailSource sortByNameAscending:self.toggleFlag];
+    CocktailDataSource *cocktailSource;
+    if ([self.dataSource isKindOfClass:[CocktailDataSource class]]) {
+        cocktailSource = (CocktailDataSource *)self.dataSource;
+    }
     
-    [self.managedDataSource sortByNameAscending:self.toggleFlag];
+    [cocktailSource sortByNameAscending:self.toggleFlag];
 }
 
 @end
